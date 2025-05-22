e83c3d0ab7624021bf86d026d9facb9a944e5160
pglima91 <pglima2024@gmail.com>
2025-05-22 15:35:05 +0000

    testes de operadores logicos

---
+++ oplogico.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 10, b = 5;
+
+    if (a > 0 || b > 0)
+    {
+        printf("Pelo menos um dos numeros é positivos\n");
+    } else {
+        printf("Os dois numero são negativos");
+    }
+
+}
---
+++ oplogico3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = -10;
+
+    if (!a > 0) { //(!(a > 0))
+    
+        printf("A variave é negativa\n");
+    } else {
+        printf("A variavel é positiva\n");
+    }
+
+}
